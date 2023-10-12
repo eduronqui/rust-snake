d513e040cd0e3d1ac20d41d26d7f624e0ec30a78
Eduardo Ronqui da Silva <edu.ronqui@gmail.com>
2023-10-12 08:47:36 +0200

    Bootstrapping the game

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use bevy::prelude::{App, Update};
+use bevy::prelude::*;
+
+const BG_GREEN: Color = Color::rgb(0.52, 0.73, 0.17);
 
 fn main() {
+    // DefaultPlugins bundles plugins for window, log and much more
     App::new()
-        .add_systems(Update, hello_world)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Snake!".to_string(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .insert_resource(ClearColor(BG_GREEN))
+        .add_systems(Startup, setup)
         .run();
 }
 
-fn hello_world() {
-    println!("Hello, bevy!");
+fn setup(mut commands: Commands) {
+    // 2D camera will be used to spawn sprites later on
+    commands.spawn(Camera2dBundle::default());
 }
